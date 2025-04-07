b86176bd6d6ca92df05121a89ad296336b83f67a
Mustapha Osman <wnfynn@gmail.com>
2025-04-07 02:07:24 +0000

    backend deployed succesfully

---
+++ StreamVibe/src/config.js
@@ -0,0 +1,13 @@
+// API Configuration
+export const API_BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://streamvibe-backend-q0e9.onrender.com/api'
+  : 'http://127.0.0.1:8000/api';
+
+// TMDB Configuration
+export const TMDB_API_KEY = '1a2db6584f1bb0b764f1bf445b503d83';
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+// Other Configuration
+export const SITE_URL = process.env.NODE_ENV === 'production'
+  ? 'https://svibe.netlify.app'
+  : 'http://localhost:5173'; 
