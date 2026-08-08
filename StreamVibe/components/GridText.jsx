d521fd18b6b80cdaa8311d9d6499ea0381b6f560
Mustapha Osman <123217645+staff2277@users.noreply.github.com>
2026-08-08 16:46:55 +0000

    Update button to prevent text wrapping

--- StreamVibe/components/GridText.jsx
+++ StreamVibe/components/GridText.jsx
@@ -16,9 +16,9 @@ const GridText = () => {
           </span>
         </p>
         <div className="flex justify-center">
-          <button className="flex gap-3 bg-[#E50000] normal-case hover:bg-[#3a3a3a] focus:bg-[#3f3f3f] active:bg-[#3f3f3f] focus:outline-none focus:ring focus:ring-[#cac8c896] max-sm:px-[2rem] max-sm:py-[1rem] rounded-lg  sm:px-[3rem] sm:py-[1.3rem] max-5s:px-[1rem] max-5s:py-[0.5rem] max-5s:text-[0.9rem] max-5s:mt-[10px] 5s:mt-8">
+          <button className="flex gap-3 whitespace-nowrap bg-[#E50000] normal-case hover:bg-[#3a3a3a] focus:bg-[#3f3f3f] active:bg-[#3f3f3f] focus:outline-none focus:ring focus:ring-[#cac8c896] max-sm:px-[2rem] max-sm:py-[1rem] rounded-lg  sm:px-[3rem] sm:py-[1.3rem] max-5s:px-[1rem] max-5s:py-[0.5rem] max-5s:text-[0.9rem] max-5s:mt-[10px] 5s:mt-8">
             <img src="/static/play.svg" alt="play-icon" />
-            <span>Start Watching Now</span>
+            <span className="whitespace-nowrap">Start Watching Now</span>
           </button>
         </div>
       </div>
